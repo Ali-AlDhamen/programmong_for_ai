005de07a728f97bb1dd7ff36fdd094407499d1be
Ali-AlDhamen <ali77dhamen@hotmail.com>
2023-09-03 15:18:51 +0300

    lab1

---
+++ lab1.py
@@ -0,0 +1,58 @@
+print("Task 1")
+a = 3.24
+b = 9e3
+c = -39j
+print(a, b, c)
+
+
+print("Task 2")
+import random
+print(random.randint(14, 24))
+
+print("Task 3")
+v = "outfit is cyan"
+print(v)
+
+print("Task 4")
+A = """
+Looking at the sky tonight,
+thinking of you by my side!
+Let the world go on and on; it will be alright if I stay strong!
+"""
+print(A)
+
+print("Task 5")
+P = "brilliant"
+print(P[3])
+
+
+print("Task 6")
+P = "strive for success!"
+print(P[3:7]) 
+
+print("Task 7")
+x = "coding is cool"
+print(x[-13:-8])
+
+print("Task 8")
+z = "programming champ"
+print(len(z))
+
+print("Task 9")
+A = "Let`s have"
+B = "some pizza!"
+print(A + " " + B)
+
+print("Task 10")
+A = "her lucky number is"
+B = 18
+print(A + " " + str(B))
+
+
+print("Task 11")
+
+X = "Just do it!"
+Y = 3.24
+print(X + " " + str(Y))
+
+
